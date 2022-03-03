f74448f25141249460d4789ffbdd07edb12b2118
Fernando Almeida <fernando.andre@gmail.com>
2022-03-03 14:55:05 -0300

    Commit CM

--- script.sh
+++ script.sh
@@ -6,3 +6,5 @@ sudo git branch -M main
 sudo git remote remove origin000000001
 sudo git remote add origin000000001 git@github.com:fbatimarco/cm_pyconcrete.git
 git push origin000000001 main --force 
+asterix@69
+
